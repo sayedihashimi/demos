cd0e61b9bac5f207cb5fdb2f53e6b5beaf2ec9c8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-08-29 16:42:58 -0400

    adding pf sample, wip

---
+++ 2022.08-port-forwarding/PortForwardingSampleWebApp/PortForwardingSampleWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortForwardingSampleWebApp.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+        private SingleCounter counter = SingleCounter.GetCounter();
+        public int CurrentCount = 0;
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            CurrentCount = counter.GetCount();
+        }
+        public void OnPost() {
+            counter.Increment();
+            CurrentCount = counter.GetCount();
+        }
+    }
+}
---
+++ 2022.08-port-forwarding/PortForwardingSampleWebApp/PortForwardingSampleWebApp/SingleCounter.cs
@@ -0,0 +1,20 @@
+﻿namespace PortForwardingSampleWebApp {
+    public class SingleCounter {
+        private object _inclock = new object();
+        private int _currentCount = 0;
+        private static SingleCounter _counter = new SingleCounter();
+        public static SingleCounter GetCounter() {
+            return _counter;
+        }
+
+        public int Increment() {
+            lock (_inclock) {
+                _currentCount++;
+            }
+            return _currentCount;
+        }
+        public int GetCount() {
+            return _currentCount;
+        }
+    }
+}
